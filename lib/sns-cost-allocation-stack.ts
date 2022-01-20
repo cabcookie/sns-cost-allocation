28e51e3b484ceaf8a1273f010ff892ac3971e18e
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-20 12:48:47 +0100

    feat: deploy a pipeline

--- lib/sns-cost-allocation-stack.ts
+++ lib/sns-cost-allocation-stack.ts
@@ -1,16 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SnsCostAllocationStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SnsCostAllocationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'SnsCostAllocationPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('cabcookie/sns-cost-allocation', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
   }
 }
