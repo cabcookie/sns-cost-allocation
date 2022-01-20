fba9ec532598835e7f47c137d29720a6413e029b
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-20 12:38:14 +0100

    feat!: initializing CDK

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # sns-cost-allocation
 
 Build a solution to allocate costs for text messages to callers.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+ * `npm run build`   compile typescript to js
+ * `npm run watch`   watch for changes and compile
+ * `npm run test`    perform the jest unit tests
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk synth`       emits the synthesized CloudFormation template
---
+++ test/sns-cost-allocation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SnsCostAllocation from '../lib/sns-cost-allocation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sns-cost-allocation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SnsCostAllocation.SnsCostAllocationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
