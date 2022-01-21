ff0ab2c69bd60f02472add73c050988de88612b6
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-21 15:20:13 +0100

    feat: trying to get a secret into the pipeline

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Function, InlineCode, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { Construct } from "constructs";
 
 
@@ -7,10 +8,18 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const phoneNumber = Secret.fromSecretNameV2(this, "PhoneNumber", "myPersonalIdentifiableInformation");
+    console.log("Secret:", phoneNumber);
+    console.log("Phone Number:", phoneNumber.secretValueFromJson('phoneNumber'));
+
     new Function(this, 'LambdaFunction', {
       runtime: Runtime.NODEJS_12_X,
       handler: 'index.handler',
       code: new InlineCode('exports.handler = _ => "Hello, CDK";'),
+      environment: {
+        SECRET_NAME: phoneNumber.secretName,
+        SECRET_VALUE: phoneNumber.secretValueFromJson('phoneNumber').toString(),
+      }
     });
   }
 }
