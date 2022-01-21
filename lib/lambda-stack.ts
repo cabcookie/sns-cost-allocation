7cee6c932509ef1c93a8d3b310dde8aafe5ea0b1
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-21 16:34:42 +0100

    feat: Lambda subscribed to an SNS topic

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,10 +1,12 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { LambdaSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
 
 
-export class LambdaStack extends Stack {
+export class HandleSnsSmsMessages extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -12,14 +14,27 @@ export class LambdaStack extends Stack {
     // console.log("Secret:", phoneNumber);
     // console.log("Phone Number:", phoneNumber.secretValueFromJson('phoneNumber'));
 
-    new Function(this, 'LambdaFunction', {
-      runtime: Runtime.NODEJS_12_X,
+    const topic = new Topic(this, "SendSmsTopic", {
+      displayName: "Request a short message delivery",
+    });
+
+    const sendSmsAndRecordCaller = new Function(this, 'SendSmsAndRecordCaller', {
+      runtime: Runtime.NODEJS_14_X,
       handler: 'index.handler',
+      timeout: Duration.seconds(15),
+      memorySize: 1024,
       code: Code.fromAsset('app'),
       environment: {
         PHONE_NUMBER: phoneNumber.secretValueFromJson('phoneNumber').toString(),
         REGION: this.region,
       }
     });
+
+    topic.addSubscription(new LambdaSubscription(sendSmsAndRecordCaller));
+
+    new CfnOutput(this, "sendSmsTopicArn", {
+      value: topic.topicArn,
+      description: "The ARN of the SNS topic to which the Lambda is subscribed to",
+    })
   }
 }
--- lib/pipeline-app-stage.ts
+++ lib/pipeline-app-stage.ts
@@ -1,11 +1,11 @@
 import { StackProps, Stage } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { LambdaStack } from "./lambda-stack";
+import { HandleSnsSmsMessages } from "./lambda-stack";
 
 export class PipelineAppStage extends Stage {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaStack = new LambdaStack(this, 'LambdaStack');
+    const lambdaStack = new HandleSnsSmsMessages(this, 'HandleSnsSmsMessagesStack');
   }
 }
