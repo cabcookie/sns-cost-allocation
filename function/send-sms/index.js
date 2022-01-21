d56ab150c32f8ea6f9ebf96812193e93a436ae0b
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-21 18:02:59 +0100

    feat: does the Lambda send an SMS now?

--- function/send-sms/index.js
+++ function/send-sms/index.js
@@ -1,15 +1,15 @@
-// const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 
 exports.handler = async (event) => {
-  // const sns = new SNSClient(process.env.REGION);
-  // const phoneNumber = process.env.PHONE_NUMBER;
+  const sns = new SNSClient(process.env.REGION || 'us-east-1');
+  const phoneNumber = process.env.PHONE_NUMBER;
 
-  // const publishCommand = new PublishCommand({
-  //   Message: "This is a test message",
-  //   PhoneNumber: phoneNumber,
-  // })
+  const publishCommand = new PublishCommand({
+    Message: "This is a test message",
+    PhoneNumber: phoneNumber,
+  })
 
-  // await sns.send(publishCommand);
+  await sns.send(publishCommand);
 
   return {
     statusCode: 200,
