f3320824299456ede17a5a82ccec542f93bd842a
Carsten Koch <carsten.b.koch@gmail.com>
2022-01-21 13:14:17 +0100

    fix: doesn't work with secrets CDK seems not to support to retrieve a secret at runtime.

--- lib/sns-cost-allocation-stack.ts
+++ lib/sns-cost-allocation-stack.ts
@@ -1,35 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { PipelineAppStage } from './pipeline-app-stage';
-
-// interface SnsCostAllocationStackProps extends StackProps {
-//   secretNames: {
-//     githubRepo: string;
-//   }
-// }
+import appConfig from '../bin/app-config';
 
 export class SnsCostAllocationStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const account = Stack.of(this).account;
-    const region = Stack.of(this).region;
-
-    const secretGithubRepoArn = `arn:aws:secretsmanager:${region}:${account}:secret:github-repo`;
-
-    const secret = Secret.fromSecretAttributes(this, "GitHubRepo", {
-      secretPartialArn: secretGithubRepoArn,
-    });
-
-    const owner = secret.secretValueFromJson('owner');
-    const repository = secret.secretValueFromJson('repository');
-
+    
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'SnsCostAllocation',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub(`${owner}/${repository}`, 'main'),
+        input: CodePipelineSource.gitHub(`${appConfig.githubRepo}`, 'main'),
         commands: [
           'npm ci',
           'npm run build',
